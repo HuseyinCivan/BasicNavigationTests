bab7dbd6fe4e9fd085b245f3b9001c193065cff8
Huseyin Civan <huseyincivan@gmail.com>
2020-03-08 00:07:53 -0500

    StringUtility edit

--- src/test/java/com/cbt/utilities/StringUtility.java
+++ src/test/java/com/cbt/utilities/StringUtility.java
@@ -4,8 +4,12 @@ public class StringUtility {
     public static void verifyEquals (String expected, String actual){
         if(expected.equalsIgnoreCase(actual)){
             System.out.println("PASS");
+            System.out.println("expected = " + expected);
+            System.out.println("actual = " + actual);
         }else{
             System.out.println("FAIL");
+            System.out.println("expected = " + expected);
+            System.out.println("actual = " + actual);
         }
     }
 }
